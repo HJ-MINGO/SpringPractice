0d92c5135614b5a71a114796c5f6019f19b73d24
MINGO <skrhkd7@gmail.com>
2024-06-12 00:21:24 +0900

    #2-commit
    commit

--- src/main/java/com/haejun/springpractice/SpringPracticeApplication.java
+++ src/main/java/com/haejun/springpractice/SpringPracticeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringPracticeApplication {
-
+	// 인텔리제이 경우 자바를 실행할때 직접 실행이 아닌 gradle를 통해서 실행한다.
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPracticeApplication.class, args);
 	}
